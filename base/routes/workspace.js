8983bce6dd1a1cced0017efee7af0f571c9ab7a1
Jorge Blasco <jorgeblasco0595@gmail.com>
2021-06-24 11:00:37 -0300

    add workspace routes, validations and controller L

---
+++ base/routes/workspace.js
@@ -0,0 +1,37 @@
+var express = require('express')
+var router = express.Router()
+const controller = require('../controllers/workspace')
+const { param } = require('express-validator')
+const validator = require('../middleware/validator')
+
+// user CRUD
+router.get('/', controller.index)
+
+router.post(
+    '/store',
+    validator.returnErrors,
+    controller.store
+)
+
+router.get(
+    '/show/:id',
+    param('id').custom(controller.existsId),
+    validator.returnErrors,
+    controller.show
+)
+
+router.put(
+    '/update/:id',
+    param('id').custom(controller.existsId),
+    validator.returnErrors, 
+    controller.update
+)
+
+router.delete(
+    '/delete/:id', 
+    param('id')
+        .custom(controller.existsId),
+    validator.returnErrors,
+    controller.destroy)
+
+module.exports = router;
