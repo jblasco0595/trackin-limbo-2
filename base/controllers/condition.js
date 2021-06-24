0c41d77094e2dd2e2f77448151d19e54bd6aaa7b
Jorge Blasco <jorgeblasco0595@gmail.com>
2021-06-24 11:14:35 -0300

    add condition full estructure L

---
+++ base/controllers/condition.js
@@ -0,0 +1,80 @@
+const { Condition } = require ('../models/index')
+const errors = require('../middleware/errors')
+
+const dbModel = Condition
+const dbModelMsg = "Condition"
+
+const store = (req, res, next) => {
+    dbModel.create({ 
+        ...req.body, 
+    })
+    .then(record => res.json(record))
+    .catch( error => {
+        errors.response(res, error)
+    });
+}
+
+const index = (req, res, next) => {
+    dbModel.findAll()
+    .then( response => {
+        res.status( 200 ).json( response )
+    })
+    .catch( error => {
+        errors.response(res, error)
+    })
+}
+
+const show = (req, res, next) => {
+    Record = dbModel.findOne({   
+        where: { id: req.params.id }
+    })
+    .then((record) => { 
+        res.json(record) 
+    })
+    .catch( error => {
+        errors.response(res, error)
+    });
+}
+
+const update = (req, res, next) => {
+    return dbModel.update( {
+        ...req.body,
+    }, {
+        where: { id: req.params.id }
+    })
+    .then( (updated) => {
+        if (updated) {
+            res.status( 200 ).json({msg: dbModelMsg + ' updated!'})
+        }
+    })
+    .catch( error => {
+        errors.response(res, error)
+    });
+}
+
+const destroy = (req, res, next) => {
+    dbModel.destroy({ 
+        where: { id: req.params.id }
+    });
+    res.json({ success: dbModelMsg + ' deleted!'})
+}
+
+const existsId = (value) => {
+    return dbModel.findOne({ 
+        where: { id: value }
+    })
+    .then((range) => { 
+        if (range === null) {
+            return Promise.reject('this ' + dbModelMsg + ' does not exist')
+        } 
+    })
+}
+
+module.exports = {
+    store,
+    index,
+    show,
+    update,
+    destroy,
+    existsId,
+};
